8a06e3f4ef46b5e9fe195c40b24cc5a6ed1cf1da
rchascall <robert.hascall@outlook.com>
2023-02-09 18:56:27 -0800

    commit changes

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, join
 from datetime import date
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+import pandas as pd
+import numpy as np
 
 #----------------#
 # Database Setup #
