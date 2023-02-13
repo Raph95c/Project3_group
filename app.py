799a6de3b8c89d9960f374b1c7fb7b1547c23e9a
Ray54111 <zapienraymundo@gmail.com>
2023-02-13 12:51:28 -0800

    updated the app.py to read some of the pages.

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+import cgi
+import os
+
+import jinja2
+from flask import Flask, redirect, render_template, request
+
+template_dir = os.path.join(os.path.dirname(__file__),'Pages')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/home.html')
+def home():
+   template = jinja_env.get_template('home.html')
+   return template.render()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/analysis.html')
+def analysis1():
+   template = jinja_env.get_template('analysis.html')
+   return template.render()
+
+@app.route('/Sources.html')
+def Sources():
+   template = jinja_env.get_template('Sources.html')
+   return template.render()
+
+@app.route('/Sources2')
+def Sources2():
+   template = jinja_env.get_template('Sources2.html')
+   return template.render()
+
+if __name__ == '__main__':
+   app.run(debug=True)
